e440c7d8adb2683ae22b0abf345b499e742c9ad3
igornardoni <igornardonik@gmail.com>
2023-10-26 09:30:33 -0300

    first commit

---
+++ expecificacoes_negociacao.md
@@ -0,0 +1,6 @@
+
+# ==> Uma negociação não pode ser modificada após ser criada.
+
+# ==> Obrigatoriamente tem uma data, quantidade e valor.
+
+# ==> Seu volume é calculdado, multiplicando-se a quantidade negociada no dia, pelo valor negociado.
---
+++ main.py
@@ -0,0 +1,12 @@
+# negociacao.js
+
+class Negociacao:
+    _data = ...
+    _quantidade = ...
+    _valor = ...
+    
+    def __init__(self, data, quantidade, valor):
+        self._data = data
+        self._quantidade = quantidade
+        self._valor = valor
+    
